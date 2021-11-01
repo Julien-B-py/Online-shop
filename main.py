d509f7ab007ed00440d5bbde17c3cbc70017163f
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-11-01 16:18:14 +0100

    v0.6
    Add "remove from cart" and "clear cart" functions.
    Edit CSS and HTML.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import os
+
 import pandas as pd
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 store_df = pd.read_csv("store.csv")
 store_data = store_df.to_dict('records')
@@ -18,23 +21,35 @@ def home():
 
 @app.route("/add/<item_id>")
 def add_to_cart(item_id):
-
     if item_id not in user_cart:
         user_cart[item_id] = 1
     else:
         user_cart[item_id] += 1
 
+    flash("The item was added to your shopping cart.")
+
     return redirect(url_for("home"))
+
+
+@app.route("/remove/<item_id>")
+def remove_from_cart(item_id):
+    del user_cart[item_id]
+    flash("The selected item has been removed.")
+    return redirect(url_for("cart"))
 
 
 @app.route("/clear")
 def clear_cart():
-    user_cart.clear()
-    return redirect(url_for("checkout"))
+    if user_cart:
+        user_cart.clear()
+        flash("All items have been removed.")
+    else:
+        flash("Your cart is already empty.")
+    return redirect(url_for("cart"))
 
 
-@app.route("/checkout")
-def checkout():
+@app.route("/cart")
+def cart():
     final_cart = []
     total = 0
 
@@ -50,6 +65,11 @@ def checkout():
                 total += item.get("price") * user_cart.get(_id)
 
     return render_template("cart.html", final_cart=final_cart, total=total)
+
+
+@app.route("/checkout")
+def checkout():
+    return "Checkout"
 
 
 if __name__ == "__main__":
